057028912379027a94c0e56bc9eac0b7bd1c9dfc
Aryan raj <114879450+ARYAN-RAJ08@users.noreply.github.com>
2026-04-26 01:36:39 +0530

    Time: 131 ms (50.00%) | Memory: 49.4 MB (90.91%) - LeetSync

--- 3781-maximize-the-distance-between-points-on-a-square/maximize-the-distance-between-points-on-a-square.java
+++ 3781-maximize-the-distance-between-points-on-a-square/maximize-the-distance-between-points-on-a-square.java
@@ -1,39 +1,29 @@
 import java.util.*;
-
 class Solution {
     long sideLen;
-
     private long getDist(int[] p) {
         int x = p[0], y = p[1];
-
         if (y == 0) return x;
         if (x == sideLen) return sideLen + y;
         if (y == sideLen) return 3 * sideLen - x;
         return 4 * sideLen - y;
     }
-
     private boolean canPlace(long[] arr, int k, long d) {
         int n = arr.length / 2;
-
         for (int i = 0; i < n; i++) {
             int count = 1;
             long first = arr[i];
             long last = arr[i];
             int idx = i;
-
             while (count < k) {
                 long target = last + d;
-
                 int next = Arrays.binarySearch(arr, idx + 1, i + n, target);
-
                 if (next < 0) next = -next - 1;
                 if (next >= i + n) break;
-
                 last = arr[next];
                 idx = next;
                 count++;
             }
-
             if (count == k) {
                 if ((first + 4 * sideLen) - last >= d) {
                     return true;
@@ -42,17 +32,14 @@ class Solution {
         }
         return false;
     }
-
     public int maxDistance(int side, int[][] points, int k) {
         sideLen = side;
-
         int n = points.length;
         long[] arr = new long[n];
 
         for (int i = 0; i < n; i++) {
             arr[i] = getDist(points[i]);
         }
-
         Arrays.sort(arr);
         long[] extended = new long[2 * n];
         for (int i = 0; i < n; i++) {
