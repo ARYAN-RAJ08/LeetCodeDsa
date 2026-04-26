dcbc441d6b1fb81dd261343506412349957ea3c0
Aryan raj <114879450+ARYAN-RAJ08@users.noreply.github.com>
2026-04-27 02:08:52 +0530

    Time: 2 ms (99.94%) | Memory: 45.5 MB (79.63%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+        int[] need = new int[128];
+        for (char c : t.toCharArray()) {
+            need[c]++;
+        }
+        int left = 0, right = 0;
+        int count = t.length();
+        int minLen = Integer.MAX_VALUE;
+        int start = 0; 
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            
+            if (need[c] > 0) {
+                count--;
+            }
+            need[c]--;
+            right++;
+            while (count == 0) {
+                if (right - left < minLen) {
+                    minLen = right - left;
+                    start = left;
+                }
+                char leftChar = s.charAt(left);
+                need[leftChar]++;
+                
+                if (need[leftChar] > 0) {
+                    count++;
+                }
+                left++;
+            }
+        }
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+    }
+}
